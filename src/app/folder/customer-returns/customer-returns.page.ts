30ee00ce3479ecacbc415a5202286e08353e526c
Tracy Lynn <tracy.lynn@boruapps.com>
2021-03-30 09:02:57 +0700

    48566

--- src/app/folder/customer-returns/customer-returns.page.ts
+++ src/app/folder/customer-returns/customer-returns.page.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'app-customer-returns',
   templateUrl: './customer-returns.page.html',
   styleUrls: ['./customer-returns.page.scss'],
 })
 export class CustomerReturnsPage implements OnInit {
-
-  constructor() { }
+  num: any;
+  constructor(
+      private barcodeScanner: BarcodeScanner
+  ) {}
 
   showSwalTest(){
     Swal.fire('Testing', 'Test', 'success'); //this works
   }
+  scan() {
+    this.num = null;
+    this.barcodeScanner.scan().then(data => {
+      // this is called when a barcode is found
+      this.num = data.text
+    });
+  }
 
-  ngOnInit() {
+
+
+ngOnInit() {
     
   }
 
